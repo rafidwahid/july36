c878a2c7c5cabd10a37cc75aed1226894c61e905
Rafid Wahid <rafidwahid@gmail.com>
2024-08-09 23:07:50 +0600

    fix: remove api dependency

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,30 +1,10 @@
-import { useState, useEffect } from "react";
 import { Chrono } from "react-chrono";
-import axios from "axios";
+import { items } from "./data";
 
 export default function Timeline() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "http://18.143.158.107:3000/timeline-event"
-        );
-        const apiData = response.data;
-
-        setItems(apiData);
-      } catch (error) {
-        console.error("Error fetching timeline items:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <div style={{ width: "100%" }}>
-      {items.length > 0 ? (
+      {
         <Chrono
           items={items}
           mode="VERTICAL_ALTERNATING"
@@ -38,9 +18,7 @@ export default function Timeline() {
             title: "1rem",
           }}
         />
-      ) : (
-        <p>No items to display.</p> // Provide feedback if no items are present
-      )}
+      }
     </div>
   );
 }
