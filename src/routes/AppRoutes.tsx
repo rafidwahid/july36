6fe89c581cb52bfc0d4629f0f9026573e310c366
rafid-ophs <rafidwahid@gmail.com>
2024-08-09 15:58:02 +0600

    fix: update data file, remove unused routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NewEventItems from "../pages/NewEventItems";
 import Timeline from "../pages/Timeline";
-import EditTimeline from "../pages/timeline/EditTimeline";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Timeline />} />
-        <Route path="/add_item" element={<NewEventItems />} />
-        <Route path="/timeline_item/:id" element={<EditTimeline />} />
       </Routes>
     </BrowserRouter>
   );
