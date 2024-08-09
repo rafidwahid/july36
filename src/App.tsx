1583bbc1172c335b3742979ab5e43c7ff41b2e64
Rafid Wahid <rafidwahid@gmail.com>
2024-08-09 22:58:49 +0600

    fix: remove unused router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import AppRoutes from "./routes/AppRoutes";
+// import AppRoutes from "./routes/AppRoutes";
+import Timeline from "./pages/Timeline";
 
 import "./App.css";
 
 export default function App() {
   return (
     <>
-      {/* <Timeline /> */}
-      <AppRoutes />
+      <Timeline />
+      {/* <AppRoutes /> */}
     </>
   );
 }
